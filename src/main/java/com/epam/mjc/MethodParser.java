6a7a382ced79f0d5fd7964361b2ab7c75f720f18
shariy <sharii.margarita@gmail.com>
2022-09-21 17:51:31 +0300

    commit1

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,70 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        ArrayList<String> modifiers = new ArrayList<>(Arrays.asList("private", "public"));
+        ArrayList<String> returnTypes = new ArrayList<>(Arrays.asList("String", "void", "float", "int", "double"));
+        StringTokenizer st = new StringTokenizer(signatureString, "(.)");
+
+        String[] firstPart = st.nextToken().split(" ");
+        String nameAccess = new String();
+        String nameReturn = new String();
+        String nameOfMethod = new String();
+
+        for (String names : firstPart) {
+            if (Arrays.asList(modifiers).contains(names)) {
+                nameAccess = names;
+            } else if (Arrays.asList(returnTypes).contains(names)) {
+                nameReturn = names;
+            } else {
+                nameOfMethod = names;
+            }
+        }
+        List<MethodSignature.Argument> current = new ArrayList<>();
+        if (st.hasMoreTokens()) {
+            String[] secondPart = st.nextToken().split(",");
+            for (String typo : secondPart) {
+                String[] typos = typo.split(" ");
+                MethodSignature.Argument temp = new MethodSignature.Argument(typos[0], typos[1]);
+                current.add(temp);
+            }
+        }
+        MethodSignature checkMethod = new MethodSignature(nameOfMethod, current);
+        if (nameAccess.length() > 0) {
+            checkMethod.setAccessModifier(nameAccess);
+        } else {
+            checkMethod.setAccessModifier(null);
+        }
+        if (nameReturn.length() > 0) {
+            checkMethod.setReturnType(nameReturn);
+        } else {
+            checkMethod.setReturnType(null);
+        }
+        checkMethod.setMethodName(nameOfMethod);
+        return checkMethod;
+
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,33 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class StringSplitter {
 
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
-    }
+        List<String> results = new ArrayList<>();
+        int counter = 0;
+        for (String delimiter : delimiters) {
+            if (counter == 0) {
+                Collections.addAll(results, source.split(delimiter));
+            } else {
+                List<String> temp = new ArrayList<>();
+                for (String result : results){
+                    Collections.addAll(temp, result.split(delimiter));
+                }
+                results.clear();
+                results.addAll(temp);
+            }
+            counter++;
+        }
+        results.removeAll(Arrays.asList("", null));
+        return results;
+}
 }
